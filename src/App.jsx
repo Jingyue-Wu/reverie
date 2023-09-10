010c53fb248dafc92d6f2c4682268bc0d4230907
Jingyue Wu <75918217+Jingyue-Wu@users.noreply.github.com>
2023-09-09 21:56:53 -0400

    Add background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import './App.css';
 
+import { useState } from 'react';
+
+
 function App() {
+  const backgroundList = [
+    "/environments/astro.gif",
+    "/environments/starry-night.gif",
+  ];
+
+  let i = 0;
+
+  const [background, setBackground] = useState(backgroundList[i]);
+
+  const cycleForward = () => {
+    if (i === backgroundList.length - 1) {
+      i = 0;
+    }
+
+    else {
+      i++;
+    }
+    setBackground(backgroundList[i]);
+  };
+
+
+  const cycleBackward = () => {
+    if (i === 0) {
+      i = backgroundList.length - 1;
+    }
+
+    else {
+      i--;
+    }
+    setBackground(backgroundList[i]);
+  };
 
   return (
     <>
-      <h1 className='text-3xl'>hi</h1>
+      <img className='w-[56vh] h-[60vh%]' src={background} alt="" />
+
+      <h1 className='underline hover:cursor-pointer select-none' onClick={cycleForward}> right </h1>
+      <h1 className='underline hover:cursor-pointer select-none' onClick={cycleBackward}> left</h1>
     </>
   );
 }
